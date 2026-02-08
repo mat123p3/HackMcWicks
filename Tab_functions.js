85b6629ad9f47a5ea8ae8f5e749acd627741f8b1
Matteo <matdiramio@outlook.com>
2026-02-07 19:00:47 -0500

    full change for vertical tabs

--- Tab_functions.js
+++ Tab_functions.js
@@ -1,14 +1,21 @@
-function openTrust(evt, Analysis){
-    var i, Analysis, tablinks;
-    Analysis = document.getElementsByClassName("Analysis");
-        for (i = 0; i < Analysis.length; i++) {
-        Analysis[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(InfoType).style.display = "block";
-    document.getElementById("defaultOpen").click();
-    evt.currentTarget.className += " active";
+// Tab_functions.js
+function openTrust(evt, tabName) {
+  let i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("defaultOpen").click();
+});
